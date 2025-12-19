74f6133a3e5834ca2aaff03fbcf84d333159b196
Gurpartap Singh <singhgurpartap4142@gmail.com>
2025-12-19 15:23:33 +0530

    update

--- server/swagger/swagger.js
+++ server/swagger/swagger.js
@@ -1,13 +1,31 @@
+// const path = require("path");
+// const fs = require("fs");
+// const swaggerUi = require("swagger-ui-express");
+
+
+// const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname,"swagger.json"),"utf-8"));
+// // const swaggerPath = path.join(__dirname, "swagger.json");
+// // const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, "utf8"));
+
+// module.exports = function setupSwagger(app){
+//   app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument));
+//   console.log("swagger url:http://localhost:9090/api-docs");
+// }
+
 const path = require("path");
 const fs = require("fs");
 const swaggerUi = require("swagger-ui-express");
 
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "swagger.json"), "utf-8")
+);
 
-const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname,"swagger.json"),"utf-8"));
-// const swaggerPath = path.join(__dirname, "swagger.json");
-// const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, "utf8"));
+module.exports = function setupSwagger(app) {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-module.exports = function setupSwagger(app){
-  app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument));
-  console.log("swagger url:http://localhost:9090/api-docs");
-}
+  const baseUrl =
+    process.env.RENDER_EXTERNAL_URL ||
+    `http://localhost:${process.env.PORT || 9090}`;
+
+  console.log(`Swagger Docs: ${baseUrl}/api-docs`);
+};
